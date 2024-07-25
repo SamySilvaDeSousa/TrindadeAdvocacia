1166933ab4440d28a74f08a773258ac4176587a5
Samy Silva <samysilvadesousa@gmail.com>
2024-07-25 19:42:16 -0300

    Melhoria no Menu Mobile

--- src/js/menuMobile.js
+++ src/js/menuMobile.js
@@ -1,9 +1,10 @@
-const menuHamburger = document.querySelector('#menu-hamburger');
-const navbarMobile = document.querySelector('#navbar-mobile');
-const headerOverlay = document.querySelector('#header-overlay');
+const menuHamburger = document.querySelector('#menu-hamburger')
+const navbarMobile = document.querySelector('#navbar-mobile')
+const headerOverlay = document.querySelector('#header-overlay')
 function toggleNavbar() {
-    navbarMobile.classList.toggle('hidden');
-    headerOverlay.classList.toggle('hidden');
-    menuHamburger.classList.toggle('open');
+    navbarMobile.classList.toggle('hidden')
+    headerOverlay.classList.toggle('hidden')
+    menuHamburger.classList.toggle('open')
 }
-menuHamburger.addEventListener('click', toggleNavbar);
+menuHamburger.addEventListener('click', toggleNavbar)
+headerOverlay.addEventListener('click', toggleNavbar)
