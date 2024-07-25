04e4b3fabbc8cda6a01de282a8f992f862810f5c
Samy Silva <samysilvadesousa@gmail.com>
2024-07-25 19:28:12 -0300

    Criação do Menu Mobile

---
+++ src/js/menuMobile.js
@@ -0,0 +1,9 @@
+const menuHamburger = document.querySelector('#menu-hamburger');
+const navbarMobile = document.querySelector('#navbar-mobile');
+const headerOverlay = document.querySelector('#header-overlay');
+function toggleNavbar() {
+    navbarMobile.classList.toggle('hidden');
+    headerOverlay.classList.toggle('hidden');
+    menuHamburger.classList.toggle('open');
+}
+menuHamburger.addEventListener('click', toggleNavbar);
